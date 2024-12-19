87baec804dca3a1641dd1fa3454cee0b89ef3e24
erikscar <erikscarcela@gmail.com>
2024-12-19 12:17:20 -0300

    Add all Routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,19 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './features/home/home.component';
 import { RegisterComponent } from './features/auth/register/register.component';
 import { LoginComponent } from './features/auth/login/login.component';
+import { DailiesComponent } from './features/dailies/dailies.component';
+import { CalendarComponent } from './features/calendar/calendar.component';
+import { FaqComponent } from './features/faq/faq.component';
+import { ProfileComponent } from './features/profile/profile.component';
+import { SettingsComponent } from './features/settings/settings.component';
 
 export const routes: Routes = [
-    { path: 'register', component: RegisterComponent},
     { path: '', component: LoginComponent},
-    { path: 'home', component: HomeComponent}
+    { path: 'register', component: RegisterComponent},
+    { path: 'home', component: HomeComponent},
+    { path: 'dailies', component: DailiesComponent},
+    { path: 'calendar', component: CalendarComponent},
+    { path: 'faq', component: FaqComponent},
+    { path: 'profile', component: ProfileComponent},
+    { path: 'settings', component: SettingsComponent}
 ];
---
+++ src/app/features/dailies/dailies.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dailies',
+  standalone: true,
+  imports: [],
+  templateUrl: './dailies.component.html',
+  styleUrl: './dailies.component.css'
+})
+export class DailiesComponent {
+
+}
